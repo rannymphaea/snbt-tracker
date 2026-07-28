785242725dab89317a9045ea5d3698bd4a94f2f3
rannymphaea <281074032+rannymphaea@users.noreply.github.com>
2026-07-28 10:32:27 +0700

    fix: set compileSdk=36 for file_picker compatibility

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.snbt.snbt_tracker"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
